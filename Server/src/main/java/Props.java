6f4798483e3a33819db650d016e47bde0b09f6ff
Vladislav Likhachev <mymindissharp@gmail.com>
2021-10-24 21:29:00 +0300

    Initial commit

---
+++ Server/src/main/java/Props.java
@@ -0,0 +1,44 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Props {
+    private String port;
+    private String host;
+    public static String FILEPATH = "settings.xml";
+    public static String PORT_TAG = "port";
+    public static String HOST_TAG = "host";
+
+
+    public void saveSettings() {
+        Properties saveProps = new Properties();
+        saveProps.setProperty(PORT_TAG, Integer.toString(8080));
+        saveProps.setProperty(HOST_TAG, "localhost");
+        try {
+            saveProps.storeToXML(new FileOutputStream(FILEPATH), "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void loadSettings() {
+        Properties loadProps = new Properties();
+        try {
+            loadProps.loadFromXML(new FileInputStream(FILEPATH));
+            port = loadProps.getProperty(PORT_TAG);
+            host = loadProps.getProperty(HOST_TAG);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+}
